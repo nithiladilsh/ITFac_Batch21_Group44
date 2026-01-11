faefb42ebb9c3f7cd7132046e4e22d7f379affc5
Nithila Dilshika Mendis <nithilamendis123@gmail.com>
2026-01-11 15:08:44 +0530

    Added GitHub Actions workflow and Allure reporting config

--- Test Suite Folder/cypress.config.js
+++ Test Suite Folder/cypress.config.js
@@ -2,12 +2,16 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { allureCypress } = require("allure-cypress/reporter"); // <--- 1. ADD THIS IMPORT
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
+
+      allureCypress(on, config);
+
       on(
         "file:preprocessor",
         createBundler({
